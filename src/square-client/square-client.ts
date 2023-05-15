c9b81279fb043156e601be465e5e533ef3578eb1
Matthew McKenzie <matthew.mckenzie@aa.com>
2023-05-14 21:59:41 -0700

    changed env variable access

--- src/square-client/square-client.ts
+++ src/square-client/square-client.ts
@@ -7,6 +7,7 @@ export class SquareClient {
     squareClient: Client;
   
     constructor(configService: ConfigService) {
+      console.log(process.env.SQUARE_ACCESS_TOKEN)
       this.squareClient = new Client({
         accessToken: process.env.SQUARE_ACCESS_TOKEN || configService.get<string>('SQUARE_ACCESS_TOKEN'),
         environment: process.env.NODE_ENV ? Environment.Production : Environment.Sandbox
